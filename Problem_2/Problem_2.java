686f9bb53e9355103b167d5f5b7208f0aec5372e
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-06-14 10:35:26 -0500

    Update Problem_2.java

--- Problem_2/Problem_2.java
+++ Problem_2/Problem_2.java
@@ -6,5 +6,43 @@ class Solution {
         return climbStairs(n-1)+climbStairs(n-2);
     }
   #solution 2
-  
+
+    public int climb(int n,int[] arr){
+        if(arr[n]!=-1) return arr[n];
+        arr[n] = climb(n-1,arr)+climb(n-2,arr);
+        return arr[n];
+    }
+   
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        for(int i=0;i<=n;i++){
+            arr[i]=-1;
+        }
+        arr[0]=1;
+        arr[1]=1;
+                if(arr[n]!=-1) return arr[n];
+
+        arr[n] = climb(n,arr);
+        return arr[n];
+    }
+
+  #solution 3
+
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+        int cur = 0;
+
+        for (int i = 3; i < n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;        
+    }
+
+
 }
