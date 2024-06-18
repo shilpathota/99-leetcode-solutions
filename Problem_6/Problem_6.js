959aa06ca3225c8a6ce830bc661d97c83a314f1c
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-06-18 14:44:02 -0500

    Create Problem_6.js

---
+++ Problem_6/Problem_6.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let i= digits.length-1;
+    while(digits[i]==9){
+        if(i==0){
+            let newarr = Array.from(new Array(digits.length+1), (x,i) => 0);
+            newarr[0]=1;
+            return newarr;
+        }
+        digits[i]=0;
+        i-=1;
+    }
+    digits[i]+=1;
+    return digits;
+};
