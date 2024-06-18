3621f241d1daf5f543e33f07d309e5c285456a89
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-06-18 13:16:40 -0500

    Create Problem_5.ts

---
+++ Problem_5/Problem_5.ts
@@ -0,0 +1,25 @@
+function romanToInt(s: string): number {
+         const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currValue = romanValues[s[i]];
+
+        if (i < s.length - 1 && currValue < romanValues[s[i + 1]]) {
+            result -= currValue;
+        } else {
+            result += currValue;
+        }
+    }
+
+    return result;
+};
