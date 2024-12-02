edfa8d29653f7414b8ef3135b6ae1c4f389ad5d8
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-12-01 21:13:46 -0600

    Create Problem_41.java

---
+++ Problem_41/Problem_41.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0]=1;
+        //storing prefixes
+        for(int i=1;i<nums.length;i++){
+                output[i] = output[i-1]*nums[i-1];
+        }
+        //multiply by postfixes
+        int postfix = 1;
+        for(int i=nums.length-1;i>=0;i--){          
+            output[i] *=postfix;
+            postfix*=nums[i];
+        }
+        return output;
+    }
+}  
